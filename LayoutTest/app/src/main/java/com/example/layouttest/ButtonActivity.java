d058feaa3971f1061f878d3f9c8a2226181e4661
TunaHG <asdf0185@naver.com>
2020-03-18 13:45:16 +0900

    Update Event Handler with Anonymous Inner Class

--- LayoutTest/app/src/main/java/com/example/layouttest/ButtonActivity.java
+++ LayoutTest/app/src/main/java/com/example/layouttest/ButtonActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 // Event 처리
 // Event : 사용자 혹은 시스템에 의해서 발생되는 모든 것
@@ -19,18 +20,29 @@ import android.widget.Button;
 // 3. Event : 발생된 Event에 대한 세부 정보를 가지고 있는 객체
 // Event Source에 Event Handler를 부착시켜서 Event가 발생되면 부착된 Handler를 통해서 Event를 처리
 public class ButtonActivity extends AppCompatActivity {
+    TextView myTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
 
+        // 0. TextView 객체에 대한 Reference 가져오기
+        myTV = findViewById(R.id.myTV);
         // 1. Event Source 객체 가져오기 (ID를 이용)
         Button myBtn = findViewById(R.id.eventBtn);
+
         // 2. Event Handler 객체 생성
-        MyEventHandler handler = new MyEventHandler();
+//        MyEventHandler handler = new MyEventHandler(myTV);
         // 3. Event Source에 Handler 부착
-        myBtn.setOnClickListener(handler);
+//        myBtn.setOnClickListener(handler);
+        myBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // myTV가 Member Variable이기 때문에 Error
+                myTV.setText("버튼이 클릭됐다!!!");
+            }
+        });
     }
 }
 
@@ -38,11 +50,27 @@ public class ButtonActivity extends AppCompatActivity {
 // 이벤트를 처리할 수 있는 특수한 능력을 가지고 있는 Listener 객체가 필요
 // 다시 말하면, 우리가 작성하는 Class는 특수한 Interface를 구현한 Class가 되어야 한다.
 // 특수한 Interface는 여러 개가 존재한다. (이벤트 종류에 따라서 여러개 존재)
-class MyEventHandler implements View.OnClickListener {
+//class MyEventHandler implements View.OnClickListener {
+//
+//    private TextView tv;
+//
+//    MyEventHandler() {
+//
+//    }
+//
+//    MyEventHandler(TextView tv) {
+//        this.tv = tv;
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        // 이벤트 처리코드를 작성
+////        Log.i("MYEVENT", "Button Click");
+//        // TextView의 내용을 변경
+//        tv.setText("Button Clicked");
+//    }
+//}
 
-    @Override
-    public void onClick(View v) {
-        // 이벤트 처리코드를 작성
-        Log.i("MYEVENT", "Button Click");
-    }
-}
+// 열심히 Event Handler Class를 만들었는데,
+// 실제 구현에서는 Anonymous inner class(익명의 이너클래스)를 이용하기 때문에
+// 위와 같은 외부 Class는 사용하지 않는다!
