00e80061c36e1cc4a2c7e48c9b76e4d4bb05a6e7
TunaHG <asdf0185@naver.com>
2020-03-23 11:51:19 +0900

    Update Book Search Example with Thread Example. Added Thread Setting

--- AndroidLectureExample/app/src/main/java/com/example/androidlectureexample/Example12_SimpleBookSearchActivity.java
+++ AndroidLectureExample/app/src/main/java/com/example/androidlectureexample/Example12_SimpleBookSearchActivity.java
@@ -7,10 +7,16 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Example12_SimpleBookSearchActivity extends AppCompatActivity {
 
@@ -49,7 +55,7 @@ public class Example12_SimpleBookSearchActivity extends AppCompatActivity {
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BookSearchRunnable runnable = new BookSearchRunnable();
+                BookSearchRunnable runnable = new BookSearchRunnable(handler, searchTitle.getText().toString());
                 Thread t = new Thread(runnable);
                 t.start();
             }
@@ -59,8 +65,60 @@ public class Example12_SimpleBookSearchActivity extends AppCompatActivity {
 
 // Thread를 생성하기 위해 Runnable Interface를 구현한 Class를 정의
 class BookSearchRunnable implements Runnable {
+    private Handler handler;
+    private String keyword;
+
+    BookSearchRunnable() { }
+
+    BookSearchRunnable(Handler handler, String keyword) {
+        this.handler = handler;
+        this.keyword = keyword;
+    }
+
     @Override
     public void run() {
+        // Web Application을 호출
+        // 결과를 받아와서 그 결과를 예쁘게 만든 후 Handler를 통하여 Activity에게 전달
 
+        // 1. 접속할 URL 준비
+        String url = "http://192.168.10.5:8080/bookSearch/searchTitle?keyword=" + keyword;
+        // 2. Java Network 기능은 Exception 처리를 기본으로 해야한다.
+        try {
+            // 3. URL 객체를 생성 (Java의 URL Class)
+            URL obj = new URL(url); // URL 객체의 Method를 사용하기 위해 객체로 만든다.
+            // 4. URL 객체를 이용해서 접속을 시도
+            HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+            // 5. Web Application의 호출방식을 설정 (GET, POST)
+            con.setRequestMethod("GET");
+            // 6. 정상적으로 접속이 되었는지 확인
+            //    HTTP Protocol로 접속할 때 접속 결과에 대한 상태값
+            //    202 : 접속성공, 403 : Forbidden, 404 : Not Found, 500 : Internal Server Error
+            int responseCode = con.getResponseCode();
+            Log.i("BookSearch", "서버로부터 전달된 Code: " + responseCode);
+            // 7. 네트워크 보안설정
+            //    Android App은 특정 기능을 사용하기 위해 보안을 설정해야 한다.
+            //    Android 9(Pie)버전부터는 보안이 강화됨
+            //      웹 프로토콜에 대한 기본 Protocol이 HTTP에서 HTTPS로 변경
+            //    따라서 HTTP Protocol을 이용하는 경우 AndroidManifest.xml에 설정을 추가해야한다.
+
+            // 8. 서버와의 연결객체를 이용해서 서버와의 데이터 통로를 열어야 한다. (Java Stream)
+            //    이 연결통로를 통해 데이터를 읽어들일 수 있다.
+            //    기본적인 연결통로(InputStreamReader)를 이용해서 조금 더 효율적인 연결통로(BufferedReader)로 다시 만들어 사용
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            // 서버가 보내주는 데이터를 읽어서 하나의 문자열로 만든다.
+            String readLine = "";
+            StringBuffer responseTxt = new StringBuffer();
+            while((readLine = br.readLine()) != null){
+                responseTxt.append(readLine);
+            }
+            br.close(); // 통로 사용이 끝났으니 해당 Resource 자원을 해제
+
+            // 서버로부터 얻어온 문자열을 Log로 출력
+            Log.i("BookSearch", "얻어온 내용은 : " + responseTxt.toString());
+
+            // 가져온 데이터(문자열)를 자료구조화 시켜서 안드로이드 앱에 적용하는 방법
+        } catch (Exception e){
+            Log.i("BookSearch", e.toString());
+        }
     }
 }
